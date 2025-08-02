c4b494c8bd5544c1a9e43c6f6033b4f9b5937bd8
flerim <101806062+flerim@users.noreply.github.com>
2025-08-03 01:05:55 +0200

    Dev configurable logging (#23)
    * Log requests to sublime console
    
    * Make logging configurable
    
    * Enable configurable file logging
    
    * Update default setting and bump dev version
    
    ---------

--- GAI.py
+++ GAI.py
@@ -1,11 +1,19 @@
 import sublime
 import sublime_plugin
-
+import os
 import json
 import http.client
 import threading
 from time import sleep
 from abc import abstractmethod
+import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a formatter and attach it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 class code_generator(sublime_plugin.TextCommand):
@@ -86,7 +94,7 @@ class configurator():
         self.base_obj = base_obj
         self.__section_cursor__ = section_name
 
-        self.cancelled = False  # Add this flag
+        self.cancelled = False  
 
         # Read Sublime Text configuration object
         self.source_config = {}
@@ -137,7 +145,7 @@ class configurator():
 
         def on_done(index):
             if index == -1:
-                self.cancelled = True  # Cancelled
+                self.cancelled = True  
             else:
                 configs_list = ["__default__"]
                 configs_list += list(alternates.keys())
@@ -211,7 +219,6 @@ class base_code_generator(code_generator):
         data_container = {"text": None, "data": None}
 
         def async_prepare():
-            # async_prepare the request
             code_prompt = config_handle.get_prompt()
             code_instruction = self.additional_instruction()
             user_code_content = "{} {} {}".format(
@@ -238,6 +245,10 @@ class base_code_generator(code_generator):
             data_container["data"] = data
             data_container["text"] = text
 
+            log_level = config_handle.get("log_level", "requests")
+            if log_level in ["requests", "all"]:
+                logger.info("Request Data: %s", json.dumps(data, indent=4))
+
         prepthread = threading.Thread(target=async_prepare)
         prepthread.start()
 
@@ -249,20 +260,10 @@ class base_code_generator(code_generator):
 
     @ abstractmethod
     def code_generator_settings(self):
-        """
-        Abstract method to be implemented by child classes. Should return the
-        settings for the code generator.
-        """
         pass
 
     @ abstractmethod
     def additional_instruction(self):
-        """
-        Abstract method to be implemented by child classes. Should return any
-        additional instructions for the code generation.
-
-        :return: An empty string by default.
-        """
         return ""
 
 
@@ -322,35 +323,57 @@ class async_code_generator(threading.Thread):
     result = None
 
     def __init__(self, region, config_handle, data_handle):
-        """
-        Args:
-            Key (str): The specific user's API key provided by Open-AI.
-
-            Prompt (str): The code or text string that GPT3 will manipulate.
-
-            Region (str): The highlighted area in sublime-text that we are
-            examining and where the result will be placed.
-
-            Instruction (str, optional): An instruction is required for the
-            edit endpoint, such as "translate this code to JavaScript". If
-            only code generation is needed, leave it as None.
-
-        Returns:
-            None
-        """
         super().__init__()
 
         self.region = region
         self.config_handle = config_handle
         self.data_handle = data_handle
 
+        self.logging_file_handler = None
+
     def run(self):
         self.running = True
+        self.setup_logs()
         if not self.config_handle.is_cancelled():
             self.result = self.get_code_generator_response()
         else:
             self.result = []
+
+        if self.logging_file_handler is not None:
+            self.logging_file_handler.close()
+            logger.removeHandler(self.logging_file_handler)
+        
         self.running = False
+
+    def setup_logs(self):
+
+        def stream_handler_added():
+            return any(isinstance(handler, logging.StreamHandler) 
+                for handler in logger.handlers)
+
+        def same(cur_handler, lfile):
+            cur_file = os.path.abspath(cur_handler.baseFilename)
+            return cur_file == os.path.abspath(lfile)
+
+        def file_handler_added(logfile):
+            return any(isinstance(handler, logging.FileHandler) 
+                and same(handler, logfile) for handler in logger.handlers)
+
+        # Add a stream handler if not already defined given configuration
+        if self.config_handle.get("log_level", None) is not None:
+            if not stream_handler_added():
+                stream_handler = logging.StreamHandler()
+                stream_handler.setFormatter(formatter)
+                logger.addHandler(stream_handler)
+
+        # Add a file handler if not already present given configuration
+        file_log_io = self.config_handle.get("log_file", None)
+        
+        if file_log_io is not None and not file_handler_added(file_log_io):
+            file_handler = logging.FileHandler(file_log_io)
+            self.logging_file_handler = file_handler
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
 
     def get_code_generator_response(self):
 
@@ -370,9 +393,23 @@ class async_code_generator(threading.Thread):
         }
         data = json.dumps(self.data)
 
+        log_level = self.config_handle.get("log_level", None)
+
+        if log_level in ["requests", "all"]:
+            logger.info("Request Headers: %s", json.dumps(headers, indent=4))
+            logger.info("Request Data: %s", json.dumps(self.data, indent=4))
+
         connection.request('POST', self.endpoint, body=data, headers=headers)
         response = connection.getresponse()
+
+        if log_level in ["all"]:
+            logger.info("Response Status: %s", response.status)
+            logger.info("Response Headers: %s", json.dumps(dict(response.headers), indent=4))
+
         response_dict = json.loads(response.read().decode())
+
+        if log_level in ["all"]:
+            logger.info("Response Data: %s", json.dumps(response_dict, indent=4))
 
         if response_dict.get('error', None):
             raise ValueError(response_dict['error'])
@@ -388,30 +425,8 @@ class async_code_generator(threading.Thread):
 
 
 class replace_text_command(sublime_plugin.TextCommand):
-    """
-    A Sublime Text command class that replaces a specified region of text with
-    new text.
-
-    Attributes:
-        view (sublime.View): The view where the command is executed.
-    """
 
     def run(self, edit, region, text):
-        """
-        The main method that is run when the command is executed.
-
-        Args: edit (sublime.Edit): The edit token used to group changes into a
-            single undo/redo operation.
-
-            region (tuple): A tuple representing the region of text to be
-            replaced.
-
-            text (str): The new text that will replace the old text in the
-            specified region.
-
-        Returns:
-            None
-        """
         region = sublime.Region(*region)
         self.view.replace(edit, region, text)
 
@@ -435,4 +450,3 @@ class edit_gai_plugin_settings_command(sublime_plugin.ApplicationCommand):
         new_window.focus_group(1)
         new_window.run_command(
             'open_file', {'file': '${packages}/User/gai.sublime-settings'})
-
