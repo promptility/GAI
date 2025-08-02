a970b99e0d3f30b5224a1e58891397f08702a94f
Leonidas Georgopoulos <>
2025-08-02 23:37:32 +0200

    Log requests to sublime console

--- GAI.py
+++ GAI.py
@@ -238,6 +238,10 @@ class base_code_generator(code_generator):
             data_container["data"] = data
             data_container["text"] = text
 
+            # Log the request data
+            print("Request Data:")
+            print(json.dumps(data, indent=4))
+
         prepthread = threading.Thread(target=async_prepare)
         prepthread.start()
 
@@ -370,9 +374,25 @@ class async_code_generator(threading.Thread):
         }
         data = json.dumps(self.data)
 
+        # Log the request headers and data
+        print("Request Headers:")
+        print(json.dumps(headers, indent=4))
+        print("Request Data:")
+        print(json.dumps(self.data, indent=4))
+
         connection.request('POST', self.endpoint, body=data, headers=headers)
         response = connection.getresponse()
+
+        # Log the response status and headers
+        print("Response Status:", response.status)
+        print("Response Headers:")
+        print(json.dumps(dict(response.headers), indent=4))
+
         response_dict = json.loads(response.read().decode())
+
+        # Log the response data
+        print("Response Data:")
+        print(json.dumps(response_dict, indent=4))
 
         if response_dict.get('error', None):
             raise ValueError(response_dict['error'])
@@ -435,4 +455,3 @@ class edit_gai_plugin_settings_command(sublime_plugin.ApplicationCommand):
         new_window.focus_group(1)
         new_window.run_command(
             'open_file', {'file': '${packages}/User/gai.sublime-settings'})
-
