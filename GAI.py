97f339efbdeb51fda4266365b852f234a2732ebe
Your Name <you@example.com>
2025-08-25 22:40:00 +0000

    Removing unecessary spec code

---
+++ GAI.py
@@ -0,0 +1,22 @@
+import sublime_plugin
+
+from GAI.src.commands import ( 
+    gai_generate_text_command, 
+    gai_replace_text_command, 
+    gai_edit_plugin_settings_command 
+    )
+
+class gai_generate_text_command(sublime_plugin.TextCommand):
+    pass
+
+def plugin_loaded():
+    print("GAI loaded!")
+
+    # from .src.utils.logging import init_logger
+    # init_logger()
+
+
+def plugin_unloaded():
+    print("GAI unloaded!")
+    # from .src.utils.logging import shutdown_logger
+    # shutdown_logger()
