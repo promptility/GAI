f8860911d25a4ea01ec88994af6e7664eda5f572
Your Name <you@example.com>
2025-08-25 18:55:46 +0200

    Extending structure

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
-import sys
-import os
+import sublime
+import sublime_plugin
+
 
 # Import all commands and classes directly from their modules
 # This makes them available at the top level for Sublime Text
 from .GAI.commands import gai_generate_text_command
 from .GAI.commands import gai_replace_text_command
 from .GAI.commands import gai_edit_plugin_settings_command
-from .GAI.core import generate_code_generator
-from .GAI.core import write_code_generator
-from .GAI.core import complete_code_generator
-from .GAI.core import whiten_code_generator
-from .GAI.core import edit_code_generator
-from .GAI.instruction import instruction_input_handler
+
+
+def plugin_loaded():
+    print("GAI loaded!")
+
+def plugin_unloaded():
+    print("GAI unloaded!")
+
