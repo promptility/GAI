b9dbe83ce196afa61be04ed21f88c08d5533218f
Your Name <you@example.com>
2025-08-25 18:13:40 +0200

    Comment in package __init__.py

--- GAI/__init__.py
+++ GAI/__init__.py
@@ -4,22 +4,22 @@ GAI Sublime Text Plugin
 """
 
 # Import core components
-from .async_worker import async_code_generator, logger
+# from .async_worker import async_code_generator, logger
 # from .config import gai_config
 
-# Import commands that should be available in Sublime Text
-from .commands import gai_generate_text_command
-from .commands import gai_replace_text_command  
-from .commands import gai_edit_plugin_settings_command
+# # Import commands that should be available in Sublime Text
+# from .commands import gai_generate_text_command
+# from .commands import gai_replace_text_command  
+# from .commands import gai_edit_plugin_settings_command
 
-# Import core generators
-from .core import (
-    generate_code_generator,
-    write_code_generator,
-    complete_code_generator,
-    whiten_code_generator,
-    edit_code_generator,
-)
+# # Import core generators
+# from .core import (
+#     generate_code_generator,
+#     write_code_generator,
+#     complete_code_generator,
+#     whiten_code_generator,
+#     edit_code_generator,
+# )
 
-# Import helper
-from .instruction import instruction_input_handler
+# # Import helper
+# from .instruction import instruction_input_handler
